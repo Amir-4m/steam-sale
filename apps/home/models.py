6c01371ede74160cfa59e39f824c2e7f7eb0c285
Amir <amiri.amir30@yahoo.com>
2020-11-14 16:17:11 +0330

    home app implemented - statics and templates included

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class NextSale(models.Model):
+    SALE_LUNAR = "LUNAR"
+    SALE_SPRING = "SPRING"
+    SALE_SUMMER = "SUMMER"
+    SALE_HALLOWEEN = "HALLOWEEN"
+    SALE_AUTUMN = "AUTUMN"
+    SALE_WINTER = "WINTER"
+
+    SALE_CHOICES = (
+        (SALE_LUNAR, _('Lunar')),
+        (SALE_SPRING, _('Spring')),
+        (SALE_SUMMER, _('Summer')),
+        (SALE_HALLOWEEN, _('Halloween')),
+        (SALE_AUTUMN, _('Autumn')),
+        (SALE_WINTER, _('Winter'))
+    )
+
+    name = models.CharField(_('name'), max_length=20, choices=SALE_CHOICES, unique=True)
+    sale_date = models.DateField(_('sale date'))
+    approved = models.BooleanField(_('approved'), default=False)
+    is_enable = models.BooleanField(_('is_enable'), default=True)
